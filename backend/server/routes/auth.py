b069254a097b6eecb4d60d7e161b0882ef35a31e
Antoine F <antoine44f@gmail.com>
2025-03-21 15:45:55 -0400

    feat: improved user feedback with login errors
    * login errors are sent to client
    * added auth checking with pages requiring garmin auth
    * login redirects to home(login page) or the page requesting it

--- backend/server/routes/auth.py
+++ backend/server/routes/auth.py
@@ -8,11 +8,12 @@ from flask import (
     redirect,
     url_for,
     session,
-    Response,
 )
+from werkzeug import Response
 
 from backend.server.authenticate import custom_sso_login, mfa_authentication, Client
 from backend.server.models.forms import LoginForm, MFAForm
+from backend.server.routes.status_codes import invalid_method
 from backend.src.garmin_interaction import load_oauth_tokens, is_oauth_tokens_active
 
 login_bp = Blueprint("login_bp", __name__, url_prefix="")
@@ -20,66 +21,104 @@ logger = logging.getLogger(__name__)
 client = Client()
 
 
-def login_check():
-    if not is_oauth_tokens_active():
-        return render_template("base.html", body="401: Unauthorized"), 401
+def login_check() -> Response | None:
+    caller = request.path
+    if is_oauth_tokens_active():
+        return None
+    if not load_oauth_tokens():
+        session["caller"] = caller
+        return redirect(url_for(".login"))
 
 
 def _validate_login(
     email: str, password: str, cur_client: garth.Client = client
-) -> Response | None:
+) -> (Response | None, None | str):
     csrf_garmin = custom_sso_login(email, password, client=cur_client)
+    error = None
+
     if csrf_garmin is not None:
         logger.info(f"Successful SSO login for {email}.")
         session["csrf_garmin"] = csrf_garmin
-        return redirect(url_for(".get_mfa_code"))
+        return redirect(url_for(".mfa_handler")), None
     else:
-        logger.info(f"Unsuccessful SSO login for {email}")
+        error = f"Unsuccessful SSO login for {email}"
+        logger.info(error)
+        return None, error
 
 
 @login_bp.route("/login", methods=["GET", "POST"])
 def login():
+    match request.method:
+        case "GET":
+            return login_get()
+        case "POST":
+            return login_process()
+        case _:
+            return invalid_method()
+
+
+def login_get():
+    login_form = LoginForm()
+    if load_oauth_tokens():
+        return redirect("/", 302)
+
+    return render_template("login/login.html", form=login_form)
+
+
+def login_process() -> (Response | int | None, None | str):
     # TODO: run using HTTPS
     login_form = LoginForm()
     oauth_tokens_exist = load_oauth_tokens()
+    error = None
 
     if oauth_tokens_exist is True:
-        return render_template("base.html", body="Success", title="Login Success")
+        return 200, error
 
-    if not login_form.validate_on_submit():
-        if request.method == "POST":
-            logger.debug(f"Errors: {login_form.form_errors}")
-        return render_template("login/login.html", form=login_form)
+    elif not login_form.validate_on_submit():
+        logger.debug(f"Errors: {login_form.errors}")
+        error = login_form.errors
 
-    if oauth_tokens_exist is False:
+    elif oauth_tokens_exist is False:
         email = login_form.email.data
         password = login_form.password.data
+        resp, error = _validate_login(email, password)
+        if error is None:
+            return resp, error
 
-        _resp_or_none = _validate_login(email, password)
-        if _resp_or_none is not None:
-            return _resp_or_none
-
-    return render_template("login/login.html", form=login_form)
+    return render_template("login/login.html", form=login_form, error=error), error
 
 
 def _validate_mfa_code(mfa_code: str, cur_client: garth.Client = client) -> str | None:
     error = mfa_authentication(session["csrf_garmin"], cur_client, mfa_code)
 
-    if error is None:
-        return render_template("base.html", body="Success", title="MFA Code")
-    else:
+    if error is not None:
         logger.info(error)
+        return f"Invalid code. Enter the 6 digit MFA Code."
 
 
-@login_bp.route("/mfa_code", methods=["GET", "POST"])
-def get_mfa_code():
+@login_bp.route("/login/mfa_code", methods=["GET", "POST"])
+def mfa_handler():
+    match request.method:
+        case "GET":
+            mfa_form = MFAForm()
+            return render_template("login/mfa_code.html", form=mfa_form)
+        case "POST":
+            return get_mfa_code()
+        case _:
+            return invalid_method()
+
+
+def get_mfa_code() -> (Response, str):
     mfa_form = MFAForm()
+    error = None
+    if not mfa_form.validate_on_submit():
+        error = mfa_form.errors
+        logger.debug(f"{error}")
+    else:
+        error = _validate_mfa_code(mfa_form.mfa_code.data, client)
+        if error is None:
+            caller = session.get("caller")
+            caller = "/" if caller is None else caller
+            return redirect(caller, 302)
 
-    if request.method == "GET" or not mfa_form.validate_on_submit():
-        if request.method == "POST":
-            logger.debug(f"{mfa_form.errors}")
-        return render_template("login/mfa_code.html", form=mfa_form)
-
-    _resp_or_none = _validate_mfa_code(mfa_form.mfa_code.data, client)
-    if _resp_or_none is not None:
-        return _resp_or_none, 302
+    return render_template("login/mfa_code.html", form=mfa_form, error=error)
